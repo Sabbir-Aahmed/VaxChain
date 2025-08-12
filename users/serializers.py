e3c32a7a0433c003d8a44c3230da56757ed2bd10
nishi <sharminnishi2424@gmail.com>
2025-08-12 16:51:26 +0600

    fix serializers error

--- users/serializers.py
+++ users/serializers.py
@@ -4,13 +4,14 @@ from users.models import User
 class CreateUserSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         model = User
-        fields = {
+        fields = [
             'id', 'email', 'nid', 'role', 'password', 'first_name', 'last_name', 'contact_number' 
-        }
+        ]
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         model = User
-        fields = {
-            'id', 'email', 'nid', 'role', 'first_name', 'last_name', 'specialization', 'contact_number', 'profile_picture'
-        }
+        fields =[ 
+            'id', 'email', 'nid', 'role', 'first_name', 'last_name', 'specialization', 'contact_number', 'profile_image'
+        ]
+        read_only_fields = ['nid']
