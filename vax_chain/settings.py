e93d0df93fef97fa840a61f930e40e1ee3fb617e
nishi <sharminnishi2424@gmail.com>
2025-09-12 20:54:46 +0600

    front end domain added for email activation

--- vax_chain/settings.py
+++ vax_chain/settings.py
@@ -178,6 +178,9 @@ SWAGGER_SETTINGS = {
 }
 
 DJOSER = {
+    'EMAIL_FRONTEND_PROTOCOL': config('FRONTEND_PROTOCOL'),
+    'EMAIL_FRONTEND_DOMAIN': config('FRONTEND_DOMAIN'),
+    'EMAIL_FRONTEND_SITE_NAME': 'VaxChain',
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
